4387aca934810914ff3cca9c65c1904d32b55ace
Prank Shadow <prankshadow@gmail.com>
2024-07-01 03:12:30 +0530

    fixed db

--- db.js
+++ db.js
@@ -2,41 +2,43 @@ const { Sequelize } = require("sequelize");
 
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.POSTGRES_DATABASE,
-  "postgres",
-  process.env.POSTGRES_PASSWORD,
-  {
-    host: "localhost",
-    dialect: "postgres",
-    port: 5432,
-    logging: true,
-  }
-);
+const sequelize = new Sequelize(process.env.POSTGRES_DATABASE, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: true,
+      ca: process.env.POSTGRES_SSH_CERTIFICATE,
+    },
+  },
+});
 
 const connectToDB = async () => {
   try {
-    sequelize.authenticate();
-    console.log("Postgres DB has been connected");
-  } catch (e) {
-    console.error("Unable to connect to the database");
-  }
-};
-
-const startServer = async () => {
-  try {
     await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-    await sequelize.sync();
+    console.log("Postgres DB has been connected");
     // await sequelize.sync({ force: true }).then(() => {
-    //   console.log("Database & tables created!");
+    //   console.log("Database & tables are recreated!");
     // });
-    console.log("Database synced.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
+  } catch (e) {
+    console.error("Unable to connect to the database", e);
   }
 };
 
-startServer();
+// const startServer = async () => {
+//   try {
+//     await sequelize.authenticate();
+//     console.log("Connection has been established successfully.");
+//     await sequelize.sync();
+//     // await sequelize.sync({ force: true }).then(() => {
+//     //   console.log("Database & tables created!");
+//     // });
+//     console.log("Database synced.");
+//   } catch (error) {
+//     console.error("Unable to connect to the database:", error);
+//   }
+// };
+
+// startServer();
 
 module.exports = { sequelize, connectToDB };
